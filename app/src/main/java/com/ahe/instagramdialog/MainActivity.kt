901cafc44296de845a591cc6da27293df301f758
serhatcoskunn <serhatcoskun@anadoluhayat.com.tr>
2018-08-16 15:08:16 +0300

    test

--- app/src/main/java/com/ahe/instagramdialog/MainActivity.kt
+++ app/src/main/java/com/ahe/instagramdialog/MainActivity.kt
@@ -2,15 +2,22 @@ package com.ahe.instagramdialog
 
 import android.app.Dialog
 import android.app.PendingIntent.getActivity
+import android.content.Context
 import android.graphics.Color
+import android.graphics.Rect
 import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.util.Log
+import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewTreeObserver
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.github.mikephil.charting.charts.PieChart
 import android.widget.SeekBar
@@ -23,15 +30,18 @@ import com.github.mikephil.charting.formatter.PercentFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.github.mikephil.charting.utils.MPPointF
 
-import android.view.WindowManager
-import android.view.ViewTreeObserver.OnGlobalLayoutListener
-import kotlinx.android.synthetic.main.dialog_task_tepmlate.*
+
 
 
 class MainActivity : AppCompatActivity() {
 
 
     var mChart: PieChart?=null
+    var buttonTest: Button?=null
+    var dialog: Dialog?=null
+
+    var globalX=0
+    var globalY=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -48,16 +58,33 @@ class MainActivity : AppCompatActivity() {
 
         var textView=findViewById<TextView>(R.id.textView)
 
-        var dialog: Dialog = Dialog(this@MainActivity)
-        dialog.setContentView(R.layout.dialog_task_tepmlate)
-        dialog.setCancelable(false)
+        dialog= Dialog(this@MainActivity)
+        dialog!!.setContentView(R.layout.dialog_task_tepmlate)
+        dialog!!.setCancelable(false)
 
-        dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        val lp = dialog.window.attributes
-        lp.dimAmount = 0.9f
-        dialog.window.attributes = lp
+        //dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        //val lp = dialog.window.attributes
+        //lp.dimAmount = 0.9f
+        //dialog.window.attributes = lp
 
-        mChart= dialog.findViewById(R.id.chart1)
+        mChart= dialog!!.findViewById(R.id.chart1)
+        buttonTest= dialog!!.findViewById(R.id.buttonTest)
+
+
+
+        /*buttonTest!!.setOnTouchListener(object :View.OnTouchListener{
+            override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+                Log.d("YES","OLDU");
+                return false
+            }
+
+        })*/
+
+
+
+
+
+
 
 
 
@@ -170,12 +197,12 @@ class MainActivity : AppCompatActivity() {
 
                 if(event.action==MotionEvent.ACTION_DOWN)
                 {
-                    dialog.show()
+                    dialog!!.show()
                     Log.d("GESTURE ", "göster")
                 }
                 else if(event.action==MotionEvent.ACTION_UP)
                 {
-                    dialog.dismiss()
+                    dialog!!.dismiss()
                     Log.d("GESTURE ", "kaldır")
                 }
                 /*when (event.getAction()) {
@@ -302,7 +329,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-         mChart!!.viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
+         mChart!!.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
              override fun onGlobalLayout() {
                  if(Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
                      mChart!!!!.viewTreeObserver.removeGlobalOnLayoutListener(this);
@@ -312,14 +339,89 @@ class MainActivity : AppCompatActivity() {
 
                  val locations = IntArray(2)
                  mChart!!!!.getLocationOnScreen(locations)
-                 val x = locations[0]
-                 val y = locations[1]
+                 globalX = locations[0]
+                 globalY = locations[1]
 
-                 Log.d("konum","$x $y")
+                 //Log.d("konum","$x $y")
              }
          })
 
 
         mChart!!.invalidate()
+    }
+
+
+    /*override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        /*val x = event.x
+        val cc = getChildCount()
+        for (i in 0 until cc) {
+            val c = getChildView()
+            if (x > c.getLeft() && x < c.getRight()) {
+                return c.onTouchEvent(event)
+            }
+        }
+        return false*/
+        var x=event.x
+        var y=event.y
+
+        if(globalX<x && x<globalX+buttonTest!!.measuredWidth && globalY<y && y<globalY+buttonTest!!.measuredHeight)
+        {
+            //Toast.makeText(this@MainActivity,"YES",Toast.LENGTH_SHORT).show()
+            Log.d("YES","YES")
+        }
+
+
+
+        /*if(buttonTest!!.left<x+ (buttonTest!!.parent as View).left && x<buttonTest!!.right+ (buttonTest!!.parent as View).right && buttonTest!!.top<y+ (buttonTest!!.parent as View).top && y<buttonTest!!.bottom + (buttonTest!!.parent as View).bottom)
+        {
+            //Toast.makeText(this@MainActivity,"YES",Toast.LENGTH_SHORT).show()
+            Log.d("YES","YES")
+        }*/
+
+        var asd=buttonTest!!.parent
+
+       // Log.d("YES","YES ${buttonTest!!.left}   ${(buttonTest!!.parent as View).left}")
+        /*if(buttonTest!!.left<x+ (dialog!! as View).left && x<buttonTest!!.right && buttonTest!!.top<y && y<buttonTest!!.bottom)
+        {
+            //Toast.makeText(this@MainActivity,"YES",Toast.LENGTH_SHORT).show()
+            Log.d("YES","YES")
+        }*/
+
+
+        //mChart!!.left
+        //Log.d("dispatchTouchEvent","${event.action} left=${mChart!!.left} right=${mChart!!.right} top=${mChart!!.top} bottom=${mChart!!.bottom}")
+        return super.dispatchTouchEvent(event)
+    }*/
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+
+        Log.d("KONUM","X=${ev.x} Y=${ev.y}")
+        Log.d("KONUMRAW","X=${ev.rawX} Y=${ev.rawY}")
+        Log.d("KONUMBUTTON","X=${buttonTest!!.left} Y=${buttonTest!!.top}")
+
+        val location = IntArray(2)
+        buttonTest!!.getLocationOnScreen(location)
+        Log.d("KONUMBUTTONGLOBAL","X=${location[0]} Y=${location[1]}")
+        var buttonLeft=location[0]
+        var buttonRight=buttonLeft+buttonTest!!.measuredWidth
+
+        var buttonTop=location[1]
+        var buttonBottom=buttonTop+buttonTest!!.measuredHeight
+        var x=ev.x
+        var y=ev.y
+
+        if(buttonLeft<x && x<buttonRight && buttonTop<y && y<buttonBottom)
+        {
+            Log.d("OLDU","OLDU")
+            var v = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                v.vibrate(VibrationEffect.createOneShot(500,VibrationEffect.DEFAULT_AMPLITUDE));
+            }else
+            {
+                v.vibrate(500);
+            }
+        }
+
+        return super.dispatchTouchEvent(ev)
     }
 }
